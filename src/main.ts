1eec5a7e5567bd10292ea6263998dc4af3166ade
AnatolyOsovitny <anatoly.osovitny@outlook.com>
2025-05-08 05:52:00 +0700

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import {
+  provideHighlightOptions,
+  Highlight,
+  HighlightAuto,
+} from 'ngx-highlightjs';
+import { HighlightLineNumbers } from 'ngx-highlightjs/line-numbers';
+
+import 'highlight.js/styles/github-dark.min.css';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>ngx-highlightjs</h1>
+      
+      <pre>
+        <code [highlight]="codeForHighlight"
+              language="html"
+              lineNumbers></code>
+      </pre>
+      
+      <p>highlightjs 2</p>
+      <pre>
+        <code [highlightAuto]="codeForHighlightAuto" 
+              lineNumbers></code>
+      </pre>
+  `,
+})
+export class App {
+  codeForHighlight = `<!DOCTYPE html>  <html lang='en'> <head> <meta charset='UTF-8'> <title>Title</title> </head> <body></body></html>`;
+  codeForHighlightAuto = `<!DOCTYPE html>  <html lang='en'> <head> <meta charset='UTF-8'> <title>Title</title></head><body></body></html>  `;
+}
+
+bootstrapApplication(App, {
+  providers: [
+    provideHighlightOptions({
+      lineNumbersLoader: () => import('ngx-highlightjs/line-numbers'),
+      coreLibraryLoader: () => import('highlight.js/lib/core'),
+      languages: {
+        typescript: () => import('highlight.js/lib/languages/typescript'),
+        css: () => import('highlight.js/lib/languages/css'),
+        xml: () => import('highlight.js/lib/languages/xml'),
+      },
+    }),
+  ],
+});
